d2c5815a60da1dddba11ff9d9ca1fd53ecd4f77c
Franz Valones <franz.valones107@gmail.com>
2025-09-18 13:20:30 +0800

    v1.0.3 - added "connect with me" links

--- src/app/components/header-content.tsx
+++ src/app/components/header-content.tsx
@@ -1,4 +1,26 @@
 import { Button } from "@/components/ui/button";
+import { Dialog, DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+const datas = [
+    {
+        label: "Email",
+        inputValue: "franz.valones107@gmail.com"
+    },
+    {
+        label: "Linkedin",
+        inputValue: "https://www.linkedin.com/in/franz-dainell-valones-9717a916b/"
+    },
+    {
+        label: "Facebook",
+        inputValue: "https://www.facebook.com/franzdainell.valones"
+    },
+    {
+        label: "Github",
+        inputValue: "https://github.com/Orandafish"
+    },
+]
 
 export default function HeaderContent({ className }: { className: string }) {
     return (
@@ -7,7 +29,42 @@ export default function HeaderContent({ className }: { className: string }) {
             <p className="leading-relaxed">I'm Franz Dainell Valones, a passionate Information Technology graduate at National University Baliwag,
                 specializing in Web and Mobile Application Development. With hands-on experience as a
                 Software Engineer and Quality Assurance Engineer</p>
-            <Button className="mt-5"><div className="flex justify-center">Connect with me</div></Button>
+            <Dialog>
+                <form>
+                    <DialogTrigger asChild>
+                        <Button className="mt-5 mb-5 flex justify-center w-full">Connect with me</Button>
+                    </DialogTrigger>
+                    <DialogContent className="sm:max-w-md">
+                        <DialogHeader>
+                            <DialogTitle>Connect with me</DialogTitle>
+                            <DialogDescription>
+                                Let us build something together!
+                            </DialogDescription>
+                        </DialogHeader>
+                        {datas.map((data, key)=>(
+                            <div key={key} className="flex items-center gap-2">
+                            <div className="grid flex-1 gap-2">
+                                <Label htmlFor="link" className="">
+                                    {data.label}
+                                </Label>
+                                <Input
+                                    id="link"
+                                    defaultValue={data.inputValue}
+                                    readOnly
+                                />
+                            </div>
+                        </div>
+                        ))}
+                        <DialogFooter className="sm:justify-start">
+                            <DialogClose asChild>
+                                <Button type="button" variant="secondary">
+                                    Close
+                                </Button>
+                            </DialogClose>
+                        </DialogFooter>
+                    </DialogContent>
+                </form>
+            </Dialog>
         </div>
     )
 }
