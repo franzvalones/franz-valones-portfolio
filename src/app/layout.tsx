3073bab5378c80f947ac42ada51eb75ffdda8d0b
Franz Valones <franz.valones107@gmail.com>
2025-09-18 20:59:26 +0800

    fixing issues

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,19 +3,9 @@ import { Epilogue, Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "./components/theme-provider";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
 const epilogue = Epilogue({
   subsets: ["latin"],
 })
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Franz Dainell V. Valones",
